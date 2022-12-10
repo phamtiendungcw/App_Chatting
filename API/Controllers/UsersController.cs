11bbbf626532622de463c56a8e68780fa6119517
Phạm Tiến Dũng <56197293+phamtiendungcw@users.noreply.github.com>
2022-12-11 05:10:06 +0700

    Closes #13

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,30 +10,31 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    // Url: https://localhost:5001/api/users
     [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersController(DataContext context)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _userRepository = userRepository;
+            _mapper = mapper;
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser([FromRoute] int id)
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser([FromRoute] string username)
         {
-            return await _context.Users.FindAsync(id);
+            return await _userRepository.GetMemberAsync(username);
+
         }
 
-        [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _context.Users.ToListAsync();
+            var users = await _userRepository.GetMembersAsync();
 
-            return users;
+            return Ok(users);
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
-﻿using API.Extensions;
+﻿using API.Data;
+using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,5 +21,20 @@ app.UseAuthentication();    // Phân quyền
 app.UseAuthorization();     // Quyền truy cập
 
 app.MapControllers();
+
+// Use file Seed to DB
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUser(context);
+}
+catch (Exception e)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred during migration!");
+}
 
 app.Run();
