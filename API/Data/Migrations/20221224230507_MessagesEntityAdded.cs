7323469777dd0c1e5f67e5de1e22524209bdaa97
Phạm Tiến Dũng <56197293+phamtiendungcw@users.noreply.github.com>
2022-12-26 02:21:37 +0700

    Closes #36

--- API/Data/Migrations/20221224230507_MessagesEntityAdded.cs
+++ API/Data/Migrations/20221224230507_MessagesEntityAdded.cs
@@ -28,15 +28,15 @@ namespace API.Data.Migrations
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Message", x => x.Id);
+                    table.PrimaryKey("PK_Messages", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_Message_Users_RecipientId",
+                        name: "FK_Messages_Users_RecipientId",
                         column: x => x.RecipientId,
                         principalTable: "Users",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Restrict);
                     table.ForeignKey(
-                        name: "FK_Message_Users_SenderId",
+                        name: "FK_Messages_Users_SenderId",
                         column: x => x.SenderId,
                         principalTable: "Users",
                         principalColumn: "Id",
@@ -44,12 +44,12 @@ namespace API.Data.Migrations
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Message_RecipientId",
+                name: "IX_Messages_RecipientId",
                 table: "Messages",
                 column: "RecipientId");
 
             migrationBuilder.CreateIndex(
-                name: "IX_Message_SenderId",
+                name: "IX_Messages_SenderId",
                 table: "Messages",
                 column: "SenderId");
         }
